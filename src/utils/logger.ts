49ceea0e8f8f5288bb7677ddd84cc42c2cb910d3
noman mujahid <nomanmujahid.cs@gmail.com>
2026-09-25 11:55:37 +0500

    add: TwelveData API's

--- src/utils/logger.ts
+++ src/utils/logger.ts
@@ -34,7 +34,7 @@ class Logger {
     };
 
     // Log to console in development
-    if (config.NODE_ENV === 'development' || level === 'ERROR' || level === 'WARN') {
+    if (config.NODE_ENV === 'development' || level === 'INFO' || level === 'ERROR' || level === 'WARN') {
       const emoji = {
         INFO: '📝',
         WARN: '⚠️',
